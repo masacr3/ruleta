01784201201b541e156294dabd2c0808bb69cd62
LucasAraujo639 <lucas.ezequiel.321@gmail.com>
2023-12-29 08:10:58 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className="container">
+        <div className="ruleta">
+        </div>
+        <div className="central">
+          <img src="./assets/selector.png" alt="selector"/> 
+        </div>
+          {/* <div className="premio">{premio}</div> */}
+      </div>
+
+        
+    </>
+  )
+}
+
+export default App
