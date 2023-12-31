06c9d0ac565fe250f4089d6754f27b9bc1b775ca
masacr3 <leorhcp2030@gmail.com>
2023-12-31 00:49:03 -0300

    preparando para el deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Ruleta } from "./components/Ruleta";
 import { Selector } from "./components/Selector";
 
 function App() {
+  
+  const shuffle = (array) =>{
+    for (let i=array.length -1; i > 0; i--){
+      const j = Math.floor(Math.random() * (i+1));
+      [array[i],array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+  
   const [rotar, setRotar] = useState(0)
   const [timeAnimation, setTimeAnimation] = useState(0)
+  const [data, setData] = useState(shuffle(["coca-cola","nada","casi","Daale gato","galletitas","nose"]))
+  // const [data, setData] = useState(shuffle(["coca-cola"]))
+
+  useEffect( () =>{
+    console.log("el valor actual de data es ",data);
+  },[data]);
   
   const Lanzar = () =>{
     const maxSpin = 10
@@ -26,12 +41,22 @@ function App() {
     setTimeAnimation(spins)
   }
 
+  const Reiniciar = () => {
+    setRotar(0)
+    setData([...shuffle(data)])
+    console.log("Reinicio")
+  }
+
   return (
     <div className="app-container">
       <Selector/>
-      <Ruleta rotar={rotar} timeDuration={timeAnimation}/> 
-      <h1> Ruleta </h1>
-      <button onClick={Lanzar}>Lanzar</button>
+      <Ruleta rotar={rotar} timeDuration={timeAnimation} data={data}/> 
+      {rotar == 0 ?
+        <button onClick={Lanzar}>Lanzar</button> 
+        :
+        <button onClick={Reiniciar}>Reiniciar</button>
+      }
+      
     </div>
   )
 }
--- src/components/Ruleta.jsx
+++ src/components/Ruleta.jsx
@@ -1,24 +1,11 @@
 import { PiezaRuleta } from './PiezaRuleta'
 
-export const Ruleta = ({rotar, timeDuration}) => {
-    const data = [
-       "Nada",
-       "Nada",
-       "Coca-cola",
-       "nada",
-       "Don satur",
-       "nada",
-       "alfajaor",
-       "nada",
-       "nada",
-       "otra oportunidad",
-       "Coca cola",
-       "Mati Gato"
-      ];
+export const Ruleta = ({rotar, timeDuration, data}) => {
+  
   return (
     <div className="ruleta-container">
          <div id="ruleta"
-         style={{ transform: `rotate(${rotar}deg)`, transitionDuration :  `${timeDuration}s` }}
+         style={{ transform: `rotate(${rotar}deg)`, transitionDuration :  `${timeDuration}s` , marginBottom : "10px"}}
          >
             {data && data.map((item, index) => <PiezaRuleta key={index} text={item}/> )}
         </div>
